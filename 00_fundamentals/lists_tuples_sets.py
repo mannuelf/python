d824d1698fdaeb3d0c012bb62d78c6aabd6acbff
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-13 21:20:40 +0200

    set tuples excersize

--- 00_fundamentals/lists_tuples_sets.py
+++ 00_fundamentals/lists_tuples_sets.py
@@ -41,3 +41,15 @@ winning_numbers = {1, 3, 5, 7, 9, 11}
 print(your_lottery_numbers.intersection(winning_numbers))
 print(your_lottery_numbers.union(winning_numbers))
 print({1,2,3,4}.difference({1,2}))
+
+# Exercise
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+my_list = [20, 60, 20]
+
+# Create a tuple, called my_tuple, with a single value in it
+my_tuple = (88,)
+
+# Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 77, 9, 12}
+print(set1.intersection(set2,))

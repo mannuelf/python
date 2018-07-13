542dabe75e15207057e4d9700b5d8086c13eb719
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-13 22:16:20 +0200

    flow control

--- 00_fundamentals/if_statements.py
+++ 00_fundamentals/if_statements.py
@@ -1,6 +1,6 @@
-# should_continue = True
-# if should_continue:
-#     print("Continue...")
+should_continue = True
+if should_continue:
+    print("Continue...")
 
 
 people_you_know = ["John", "Max", "Mary"]
@@ -10,3 +10,25 @@ if person in people_you_know:
     print("You know {}!".format(person))
 else:
     print("You don't {}!".format(person))
+
+
+# Exercise
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Modify the method below to make sure only even numbers are returned.
+def even_numbers(numbers):
+    evens = []
+    for number in numbers:
+        if number % 2 == 0:
+            evens.append(number)
+    return print(evens)
+
+even_numbers(numbers)
+
+# Modify the below method so that "Quit" is returned if the choice parameter is "q".
+# Don't remove the existing code
+def user_menu(choice):
+    if choice == "a":
+        return "Add"
+    if choice == "q":
+        return "Quit"

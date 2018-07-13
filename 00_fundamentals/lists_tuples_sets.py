60e3147dbcd271e3b3a4600aa3abfb89529bfb74
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-13 21:11:48 +0200

    set operations, union, intersect and difference

--- 00_fundamentals/lists_tuples_sets.py
+++ 00_fundamentals/lists_tuples_sets.py
@@ -38,5 +38,6 @@ gradesC.add(999)
 your_lottery_numbers = {1, 2, 3, 4, 5, 6}
 winning_numbers = {1, 3, 5, 7, 9, 11}
 
-# print(your_lottery_numbers.intersection(winning_numbers))
+print(your_lottery_numbers.intersection(winning_numbers))
 print(your_lottery_numbers.union(winning_numbers))
+print({1,2,3,4}.difference({1,2}))

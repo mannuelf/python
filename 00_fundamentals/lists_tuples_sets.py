675372ffc2d1379079e2f12d0e6b1b6eb02e5e08
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-13 20:44:26 +0200

    lists, tuples, sets

---
+++ 00_fundamentals/lists_tuples_sets.py
@@ -0,0 +1,19 @@
+
+grade_one = 77
+grade_two = 80
+grade_three = 90
+print((grade_one + grade_two + grade_three) / 3)
+
+# List
+gradesA = [99, 88, 78, 55, 22, 10]
+print(sum(gradesA) / len(gradesA))
+
+# Tuple
+# Tuple are immutable, cannot increase its size
+gradesB = (99, 88, 78, 55, 22, 10)
+print(gradesB)
+
+# Sets
+# unique and unordered
+gradesC = {99, 12, 54, 52, 52}
+print(gradesC)

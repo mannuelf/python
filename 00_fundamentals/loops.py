9af5d56042be5e4420b3e761383ed1c91127b1b0
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-13 21:41:06 +0200

    looopy loooops

--- 00_fundamentals/loops.py
+++ 00_fundamentals/loops.py
@@ -8,10 +8,18 @@ my_string = "hello"
 # print(my_variable[4])
 
 # my_string is an iterable, stings/list/sets/tuples
-for character in my_string:
-    print(character)
+# for character in my_string:
+    # print(character)
 
-my_list = [1, 2, 3, 4, 5]
+# my_list = [1, 2, 3, 4, 5]
 
-for number in my_list:
-    print(number ** 2)
+# for number in my_list:
+    # print(number ** 2)
+
+user_wants_number = True
+while user_wants_number == True:
+    print(10)
+
+    user_input = input("Should we print again? (y/n) ")
+    if user_input == 'n':
+        user_wants_number = False

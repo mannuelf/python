202e8e116dc3bbfa6022ee02fb070882bf64eb04
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-14 16:45:07 +0200

    classes and objects

--- 00_fundamentals/classes_objects.py
+++ 00_fundamentals/classes_objects.py
@@ -19,8 +19,22 @@ player_one = LotteryPlayer("John")
 player_one.numbers = (9, 10, 11)
 player_two = LotteryPlayer("Jack")
 
-print(player_one.name == player_two.name)
-print(player_one.name)
-print(player_two.name)
-print(player_one.numbers)
+# print(player_one.name == player_two.name)
+# print(player_one.name)
+# print(player_two.name)
+# print(player_one.numbers)
 
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks) 
+
+anna = Student("Jose", "Unisa")
+anna.marks.append(56)
+anna.marks.append(36)
+anna.marks.append(7)
+print(anna.average())
